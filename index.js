5041bd115574af67d70f9c952aa2101a7a35f551
Chukwuemeka Nwosu <Cknwosu@gmail.com>
2023-08-31 16:11:08 +0100

    set up

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const database = require('./sqlConnection');
+
+const app = express();
+
+app.listen(3000, () => {
+console.log(`Server is up and running on 3000 ...`);
+});
+
