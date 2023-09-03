11613a60d32a9f4ba87a80b8f78c7383e450f05a
Chukwuemeka Nwosu <Cknwosu@gmail.com>
2023-09-03 13:37:32 +0100

    files

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const database = require('./db_con');
+let pug = require("pug")
+let path = require('path')
 
 const app = express();
+
+app.set(path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+
+
+app.get('/', (req, res) => res.render('home'))
 
 app.listen(3000, () => {
 console.log(`Server is up and running on 3000 ...`);
