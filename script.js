c09fea047c2cf2a0497e3c72a69fe9c12d8e3e79
Happiness <frankyhappy5050@gmail.com>
2023-09-09 23:40:28 +0100

    update

---
+++ script.js
@@ -0,0 +1,83 @@
+function toggleContact() {
+  var contactInfo = document.querySelector('.contact-info');
+  if (contactInfo.style.display === 'none' || contactInfo.style.display === '') {
+      contactInfo.style.display = 'block';
+  } else {
+      contactInfo.style.display = 'none';
+  }
+}
+
+
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+// Get the feature links and the feature details container
+const featureLinks = document.querySelectorAll('.feature-link');
+const featureDetails = document.getElementById('feature-details');
+
+// Add click event listeners to each feature link
+featureLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+
+        // Get the text content of the clicked link
+        const featureName = link.textContent;
+
+        // Replace the content of the feature details container with the feature details
+        featureDetails.innerHTML = `
+            <h2>${featureName}</h2>
+            <p>Here are the details for ${featureName}.</p>
+        `;
+
+        // Show the feature details container
+        featureDetails.style.display = 'block';
+    });
+});
+
+// Swiper initialization
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true, // Set to true if you want the slider to loop
+  autoplay: {
+    delay: 3000, // Set autoplay delay in milliseconds
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
