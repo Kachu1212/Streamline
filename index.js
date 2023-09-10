188c511c20a2e012057f7bbfc539bd6229822cbb
Chukwuemeka Nwosu <Cknwosu@gmail.com>
2023-09-10 18:18:53 +0100

    update

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const path = require('path')
 
 const app = express();
 
-app.set(path.join(__dirname, './views'))
+app.set(path.join(__dirname, './public/index.html'))
 app.set('view engine', 'pug')
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.htnl'))
-
-// const dashboardRouter = require('./routes/dashboard');
+    res.sendFile(path.join(_dirname, './public/index.htnl'));
 });
+// const dashboardRouter = require('./routes/dashboard');
 // app.use('/', dashboardRouter);
 
 app.listen(3000, () => {
